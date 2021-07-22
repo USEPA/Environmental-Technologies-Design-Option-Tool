66101fd25753c856645b97b60a91fc9171b6465b
Michael Verma <Verma.Michael@epa.gov>
2021-07-22 19:34:44 -0400

    More Form Resizing
    ^

--- VS2019_AdDesignS/ADS_VBnet_MV/EditAdsorber.Designer.vb
+++ VS2019_AdDesignS/ADS_VBnet_MV/EditAdsorber.Designer.vb
@@ -700,10 +700,10 @@
         Me.Controls.Add(Me.MainMenu1)
         Me.Cursor = System.Windows.Forms.Cursors.Default
         Me.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Location = New System.Drawing.Point(56, 73)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(685, 512)
         Me.Name = "frmEditAdsorber"
         Me.RightToLeft = System.Windows.Forms.RightToLeft.No
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
